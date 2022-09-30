851895460efdaeeb2e32d5703266e8a70170270a
COllins <collinsmuiruri77@gmail.com>
2022-09-30 18:24:13 +0300

    update

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import QuestionForm from "./QuestionForm";
 
 function QuestionList() {
+
+const [question, setQuestion] = useState([]);
+
+
+useEffect(()=>{
+  fetch("http://localhost:4000/questions")
+  .then ((resp) => resp.json())
+  .then ((data) => setQuestion(data))
+},[])
+
+function handleAddQuestion(theQuestion){
+const newQuestionArray = [...question, theQuestion];
+setQuestion(newQuestionArray);
+};
+console.log(question)
   return (
     <section>
       <h1>Quiz Questions</h1>
+      <> <QuestionForm handleQuestion={handleAddQuestion}/></>
       <ul>{/* display QuestionItem components here after fetching */}</ul>
     </section>
   );
